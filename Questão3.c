1254f6598f4fc79409bb4ca84010cf781dda0b96
joaomiranda14 <143547685+joaomiranda14@users.noreply.github.com>
2023-11-25 18:44:59 -0300

    Add files via upload

---
+++ Questão3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+
+    setlocale(LC_ALL, "portuguese");
+
+    char numeros[99][100];
+    int codigo;
+    int i;
+    int contador= 0;
+    char nomes[99][200];
+
+    printf("Menu");
+    printf("\n\n1 -Cadastrar novo contato na lista telefonica \n");
+    printf("2 - Mostrar os números cadastrados: ");
+    scanf("%d", &codigo);
+    system("cls || clear");
+
+    do
+    {
+
+        fflush(stdin);
+
+        printf("Nome: ");
+        gets(nomes[contador]);
+
+        printf("Número: ");
+        gets(numeros[contador]);
+  
+  
+    printf("\n\n1 -Cadastrar novo contato na lista telefonica \n");
+    printf("2 - Mostrar os números cadastrados: ");
+
+    printf("\nDigite a opção desejada: ");
+     scanf("%d", &codigo);
+        system("cls || clear");
+        contador++;
+
+    } while (codigo != 2);
+
+    printf("NÚMEROS CADASTRADOS:\n\n");
+    for     (i = 0; i < contador; i++)
+    {
+        printf("Nome: %s\n", nomes[i]);
+        printf("Número: %s\n\n", numeros[i]);
+    }
+
+    return 0;
+}
